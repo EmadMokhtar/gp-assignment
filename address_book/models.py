3f37c88021f4affeb6a1eb098a61da70c69719f8
Emad Mokhtar <emad.m.habib@gmail.com>
2016-10-09 01:41:13 +0300

    Add Person class and its tests

--- address_book/models.py
+++ address_book/models.py
@@ -8,3 +8,27 @@ class AddressBook(Singleton):
     groups = set()
     persons = set()
 
+class Person(object):
+    """
+    Person contact information
+    """
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.addresses = set()
+        self.emails = set()
+        self.phone_numbers = set()
+        self.in_groups = set()
+
+    def add_phone_number(self, phone_number):
+        self.phone_numbers.add(phone_number)
+
+    def add_email(self, email):
+        self.emails.add(email.lower())
+
+    def add_address(self, address):
+        self.addresses.add(address)
+
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
--- address_book/test/test_models.py
+++ address_book/test/test_models.py
@@ -10,5 +10,66 @@ class TestAddressBook(unittest.TestCase):
         address_book = models.AddressBook()
         self.assertEqual(self.address_book, address_book)
 
+class TestPerson(unittest.TestCase):
+    def setUp(self):
+        self.person = models.Person('Dummy', 'Person')
+
+    def test_initialize_person(self):
+        first_name = 'Emad'
+        last_name = 'Mokhtar'
+        person = models.Person(first_name, last_name)
+        self.assertIsNotNone(person)
+        self.assertEqual(person.last_name, last_name)
+        self.assertEqual(person.first_name, first_name)
+        self.assertIsInstance(person, models.Person)
+
+    def test_add_phone_number(self, phone_number='999-999-999'):
+        self.person.add_phone_number(phone_number)
+        self.assertIn(phone_number, self.person.phone_numbers)
+
+    def test_add_duplicate_phone_number(self, phone_number='999-999-999'):
+        self.person.add_phone_number(phone_number)
+        self.person.add_phone_number(phone_number)
+        self.assertEqual(1, len(self.person.phone_numbers))
+
+    def test_add_email(self, email='email@company.com'):
+        self.person.add_email(email)
+        self.assertIn(email, self.person.emails)
+
+    def test_add_duplicate_email(self, email='email@company.com'):
+        self.person.add_email(email)
+        self.person.add_email(email)
+        self.assertEqual(1, len(self.person.emails))
+
+    def test_add_address(self, address='Inifite Loop, Apple'):
+        self.person.add_address(address)
+        self.assertIn(address, self.person.addresses)
+
+    def test_add_duplicate_address(self, address='Inifite Loop, Apple'):
+        self.person.add_address(address)
+        self.person.add_address(address)
+        self.assertEqual(1, len(self.person.addresses))
+
+    def test_get_full_name(self):
+        self.assertEqual(self.person.get_full_name(), 'Dummy Person')
+
+    def test_get_groups_perosn_belongs_to(self):
+        family_group = models.Group('Family')
+        close_family_group = models.Group('Close Family')
+        friends_group = models.Group('Firends')
+        close_friends_group = models.Group('Firends')
+        brother = models.Person('Sam', 'Albert')
+        best_friend = models.Person('Kenneth', 'Boyce')
+        family_group.add_member(brother)
+        close_family_group.add_member(brother)
+        friends_group.add_member(best_friend)
+        close_friends_group.add_member(best_friend)
+
+        self.assertEqual(brother.in_groups,
+                         set([family_group, close_family_group]))
+        self.assertEqual(best_friend.in_groups,
+                         set([friends_group, close_friends_group]))
+
+
 if __name__ == '__main__':
     unittest.main()
