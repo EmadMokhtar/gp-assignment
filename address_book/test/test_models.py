3edc8e626920ec13a38318217259e35878146515
Emad Mokhtar <emad.m.habib@gmail.com>
2016-10-09 01:40:32 +0300

    Test if AddressBook is singleton

--- address_book/test/test_models.py
+++ address_book/test/test_models.py
@@ -1,4 +1,14 @@
 import unittest
+from address_book import models
+
+
+class TestAddressBook(unittest.TestCase):
+    def setUp(self):
+        self.address_book = models.AddressBook()
+
+    def test_address_book_is_singleton(self):
+        address_book = models.AddressBook()
+        self.assertEqual(self.address_book, address_book)
 
 if __name__ == '__main__':
     unittest.main()
